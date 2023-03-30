d8ea8a01bad2c20036d54e53e64ab027869c7dd1
Sophie Wong <42300517+sophiewongsp@users.noreply.github.com>
2023-03-30 22:08:11 +0800

    ✨ FEAT: File structure, WIP barPlot

---
+++ ict2107-flask/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ict2107_flask',
+    packages=['ict2107_flask'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
